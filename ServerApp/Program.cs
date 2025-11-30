fe02a552fcbb435bb13751f828d04fe96aa3ce3f
Miktastik <eaa24smlk@students.eaaa.dk>
2025-11-30 19:19:10 +0100

    Register VarerBeholdningRepository as singleton service
    Added VarerBeholdningRepository to the dependency injection container as a singleton to make it available throughout the application.

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,4 +1,5 @@
 using WebApplication1.Data;
+using WebApplication1.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<VarerBeholdningRepository>();
 
 var app = builder.Build();
 
