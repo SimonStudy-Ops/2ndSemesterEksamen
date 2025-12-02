fcc993ed0c7d11e75e56f9d4e9875eaa4e2278d4
Oskar2162 <eaa24ohb@students.eaaa.dk>
2025-12-02 11:11:14 +0100

    Opdateret VarerBeholdning Model

--- Core/Models/VarerBeholdning.cs
+++ Core/Models/VarerBeholdning.cs
@@ -5,12 +5,9 @@ namespace Core.Models;
 
 public class VarerBeholdning
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.Int32)]
-    public int VarerbeholdId {get; set;}
-    
-    public int Mængde {get; set;}
-    public Lokalitet Lokalitet { get; set;} // Embedded
-    public int VarerId {get; set;} // Reference, ikke embedded
-    public string VarerNavn { get; set;} // Denormaliseret for nemmere læsning af liste da Navn i Varer sjældent vil ændre sig efter det er oprettet
+    public int VarerbeholdId { get; set; }
+    public int Mængde { get; set; }
+    public Lokalitet? Lokalitet { get; set; }
+    public int VarerId { get; set; }
+    public string? VarerNavn { get; set; }
 }
