95172f9d7763f010bcd3cc68bf2688cb88362c4e
Oskar2162 <eaa24ohb@students.eaaa.dk>
2025-12-03 10:16:30 +0100

    Rettet navne i ServerApp så de kalder til Core

--- ServerApp/Models/Kategorier.cs
+++
@@ -1,9 +0,0 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace WebApplication1.Models;
-
-public class Kategorier
-{
-   public string kategoriNavn { get; set; }
-}
--- ServerApp/Models/Lokalitet.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApplication1.Models;
-
-public class Lokalitet
-{
-    public int LokationId { get; set; }
-    public string LokationNavn { get; set; }
-}
