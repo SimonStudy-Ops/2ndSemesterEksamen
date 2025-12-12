9a034d85fa415fe99cd10886fcdf0f1f29bcfcb5
Simon Mik Lundholm Kæmpegaard (baransvarlig <eaa24smlk@students.eaaa.dk>
2025-12-12 12:47:09 +0100

    Reapply "Merge pull request #39 from SimonStudy-Ops/SimonK"
    This reverts commit a07236482146db42f9d6d4504a34fd6c6fe6865a.

---
+++ ServerApp/Controllers/KategoriController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Core.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/kategorier")]
+    public class KategoriController : ControllerBase
+    {
+        private readonly KategoriRepository _repo;
+
+        public KategoriController(KategoriRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public List<Kategorier> Get()
+        {
+            return _repo.GetAll();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Kategorier kategori)
+        {
+            if (string.IsNullOrWhiteSpace(kategori.kategoriNavn))
+            {
+                return BadRequest("Kategorinavn må ikke være tomt");
+            }
+            
+            _repo.CreateKategori(kategori);
+            return Ok();
+        }
+    }
+}
