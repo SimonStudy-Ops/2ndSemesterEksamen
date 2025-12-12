5c7fcbeb8cde9dcd9284f6f725973633049a6cf4
Simon Mik Lundholm Kæmpegaard (baransvarlig <eaa24smlk@students.eaaa.dk>
2025-12-12 11:24:57 +0100

    Add KategoriRepository for MongoDB operations
    Introduces KategoriRepository to handle CRUD operations for the Kategorier collection in MongoDB, including methods to create, retrieve, and delete categories by name.

---
+++ ServerApp/Repositories/KategoriRepository.cs
@@ -0,0 +1,37 @@
+using MongoDB.Driver;
+using Core.Models;
+
+namespace WebApplication1.Repositories
+{
+    public class KategoriRepository
+    {
+        private string connectionString = "mongodb+srv://eaa24mofh_db_user:mohamed123@2ndsemestereksamen.ghi4mwz.mongodb.net/?appName=2ndsemestereksamen";
+
+        IMongoClient mongoClient;
+        IMongoDatabase database;
+        IMongoCollection<Kategorier> collection;
+
+        public KategoriRepository()
+        {
+            mongoClient = new MongoClient(connectionString);
+            database = mongoClient.GetDatabase("basement");
+            collection = database.GetCollection<Kategorier>("Kategorier");
+        }
+
+        public void CreateKategori(Kategorier kategori)
+        {
+            collection.InsertOne(kategori);
+        }
+
+        public List<Kategorier> GetAll()
+        {
+            return collection.Find(_ => true).ToList();
+        }
+
+        public void DeleteByName(string kategoriNavn)
+        {
+            var filter = Builders<Kategorier>.Filter.Eq(k => k.kategoriNavn, kategoriNavn);
+            collection.DeleteOne(filter);
+        }
+    }
+}
